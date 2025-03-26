60dafe30d7069ad0e24b4d4b38d89209c1919423
gracilenes <gracilenes876@gmail.com>
2025-03-26 19:25:17 +0000

    aula

--- Aula 2/tênis.py
+++ Aula 2/tênis.py
@@ -1,11 +1,33 @@
-class Tênis:
-    def __init__(self, valor1):
+class Tenis:
+    def __init__(self):
         self.nome = None
         self.preco = None
         self.tamanho = None
         self.cor = None
         self.modalidade = None
 
-tênis1 = Tênis("Tênis esportivo")
+tenis1 = Tenis()
+tenis1.nome = "Tênis esportivo"
+print(tenis1.nome)
 
-print(tênis1.nome)
+# Definino atributos inicias
+
+Tenis.nome = "Tênis Unissex"
+Tenis.preco = 300.0
+Tenis.tamanho = 40
+Tenis.cor = "Preto"
+Tenis.modalidade = "Esporte"
+print(nome, preco,tamanho, cor, modalidade )
+
+class Livro:
+    def __init__(self):
+        self.tipo = None
+        self.capa = None
+        self.titulo = None
+        self.preco = None
+        self.cor = None
+
+livro1 = Livro()
+livro1.nome = "Lute contra os medos, Supere as ingeruranças, Reaja e volte a ser feliz"
+print(livro1.nome)
+
