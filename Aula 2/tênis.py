04b5a3423a1e62fbf26f1ed16fda11cd7fc341ad
gracilenes <gracilenes876@gmail.com>
2025-03-26 19:28:19 +0000

    aula

--- Aula 2/tênis.py
+++ Aula 2/tênis.py
@@ -10,15 +10,6 @@ tenis1 = Tenis()
 tenis1.nome = "Tênis esportivo"
 print(tenis1.nome)
 
-# Definino atributos inicias
-
-Tenis.nome = "Tênis Unissex"
-Tenis.preco = 300.0
-Tenis.tamanho = 40
-Tenis.cor = "Preto"
-Tenis.modalidade = "Esporte"
-print(nome, preco,tamanho, cor, modalidade )
-
 class Livro:
     def __init__(self):
         self.tipo = None
@@ -30,4 +21,5 @@ class Livro:
 livro1 = Livro()
 livro1.nome = "Lute contra os medos, Supere as ingeruranças, Reaja e volte a ser feliz"
 print(livro1.nome)
+
 
