21e013cddd2bd1f19055cf088ca5839c0d86a9f5
gracilenes <gracilenes876@gmail.com>
2025-05-21 19:15:31 +0000

    atv

--- Aula 7/main.py
+++ Aula 7/main.py
@@ -13,6 +13,47 @@ class Biblioteca:
         categoria = Categoria(nome_categoria)
         self.categorias.append(Categoria)
 
-print = Biblioteca("Livros")
-print.adicionar_categoria("Membros")
-print.adicionar_categoria("Empréstimos")
+biblioteca = Biblioteca("Livros")
+biblioteca.adicionar_categoria("Membros")
+biblioteca.adicionar_categoria("Empréstimos")
+
+class Livros:
+    def __init__(self, titulo):
+        self.titulo = titulo
+        self.emprestimos = []
+
+    def adicionar_emprestimos(self, emprestimo):
+        self.emprestimos.append(emprestimo)
+
+class Membro:
+    def __init__(self, nome):
+        self.nome = nome
+        self.emprestimos = []
+
+class Emprestimo:
+    def __init__(self, livro, membro):
+        self.livro = livro
+        self.membro = membro
+        livro.adicionar_emprestimo(self)
+        membro.adicionar_emprestimo(self)
+
+class Biblioteca:
+    def __init__(self):
+        self.membros = []
+
+    def adicionar_membro(self, membro):
+        self.membros.append(membro)
+
+livro1 = Livros("Lute Supere e Reaja")
+livro2 = Livros("Mary Jane")
+
+membro1 = Membro("Gracilene")
+membro2 = Membro("Carlos")
+
+biblioteca = Biblioteca()
+biblioteca.adicionar_membro(membro1)
+biblioteca.adicionar_membro(membro2)
+
+emprestimo1 = Emprestimo(livro1, membro1)
+emprestimo2 = Emprestimo(livro2, membro1)
+emprestimo3 = Emprestimo(livro1, membro2)
